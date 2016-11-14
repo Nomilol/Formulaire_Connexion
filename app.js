8c3d3f9eaeb10e8c7af21293eeb993d6305e211d
Axel <lezinaxel@gmail.com>
2016-11-14 13:38:42 +0100

    Script js

--- app.js
+++ app.js
@@ -8,24 +8,24 @@ console.log('Start');
 			app.listeners();
 		},
 		listeners : function(){
-			$('#submit').on('click', app.recupValueInput);
+			$('#submit').on('click', app.sendToServer);
 			// $('#submit').on('click',function(event){
 			// 	event.preventDefault();
 			// });
 			app.sendToServer();
 		},
 		recupValueInput : function(){
-			var username = $('#userName').val();
-			var password = $('#password').val();
 			console.log(username);
 			console.log(password);
 		},
 		sendToServer : function(){
+			app.username = $('#userName').val();
+			app.password = $('#password').val();
 			$.ajax({
 				type : 'POST',
 				data : {
-					username : 'username',
-					password : 'password',
+					username : app.username,
+					password : app.password,
 				}, 
 				url : '/login',
 				success : function(){
