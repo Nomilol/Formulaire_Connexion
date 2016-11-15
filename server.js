8c6618a6152125564c5d7d52ddca31f850d1c3f3
Axel <lezinaxel@gmail.com>
2016-11-15 14:43:12 +0100

    Le serveur

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+
 app.use(bodyParser.urlencoded({extended : false}));
 
 app.use(express.static('public'));
+app.use(bodyParser());
 
 app.listen(2222, function(){
 	console.log('Héhé, ça marche');
@@ -14,7 +16,7 @@ app.post('/login',function(req, res){
 	var userName = 'Nomilol';
 	var password = 'jaune';
 	if(req.body.username == userName && req.body.password == password){
-		console.log('Authentification réussie');
+		res.send('/login');
 
 	}
 })
